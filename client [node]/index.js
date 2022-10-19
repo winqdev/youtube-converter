3737f1c83b7bbe30ab782806775ee8bc27859749
winqdev <103835411+winqdev@users.noreply.github.com>
2022-10-19 18:26:00 +0300

    DONE

--- client [node]/index.js
+++ client [node]/index.js
@@ -1,13 +1,13 @@
-const http = require('http'); // or 'https' for https:// URLs
+//Make sure you put link from your website at line 8
+const http = require('http');
 const fs = require('fs');
 
 const link = prompt("Link from video: ")
 
 const file = fs.createWriteStream("song.mp3");
-const request = http.get(`http://server.winqdev.xyz/mp3?URL=${link}`, function(response) {
+const request = http.get(`http://example.com/mp3?URL=${link}`, function(response) {
    response.pipe(file);
-
-   // after download completed close filestream
+   
    file.on("finish", () => {
        file.close();
        console.log("Download Completed");
